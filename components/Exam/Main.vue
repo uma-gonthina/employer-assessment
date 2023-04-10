bb1be97492b68fb281b8683e74a4b66560d4ec32
uma-gonthina <you@example.com>
2023-04-10 14:25:44 +0530

    name change

--- components/Exam/Main.vue
+++ components/Exam/Main.vue
@@ -22,7 +22,7 @@
         class="rounded-full bg-indigo-600 p-4 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         @click="openModal"
       >
-        Add Student
+        Add Employee
       </button>
     </div>
   </div>
